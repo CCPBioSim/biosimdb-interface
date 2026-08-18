3402d67c5ebef03d460fa38ba3b7a01618b09ab6
Jas Kalayan <jkalayan@gmail.com>
2026-08-18 13:25:19 +0100

    fix typos in webform instructions and remove links to aiida until ready

--- biosimdb_interface/schema/webform.py
+++ biosimdb_interface/schema/webform.py
@@ -46,19 +46,31 @@ def get_simulation_metadata():
 # ``simulation_metadata`` is loaded fresh on first access via get_simulation_metadata().
 WEBFORM_SCHEMA = {
     "data": {
-        "title": "BioSim Data Extraction & Submission Form",
+        "title": "BioSim Deposition",
         "description": "This form automatically extracts useful data that describes a molecular dynamics simulation. Whenever you share your data, include this metadata with the simulation files to make your data more findable, accessible, interoperable and reuseable (FAIR).",
-        "guidance": {
-            "label": "Please Note:",
-            "options": [
-                "To extract simulation metadata, please upload a topology and corresponding trajectory file/s. Optionally, upload an aiida archive file containing the simulation provenance, to extract further information about the simulation protocol.",
-                "Multiple trajectory files can be uploaded with a single associated topology file, both file types are required.",
-                'Various simulation file formats are accepted and read using <a href="https://www.mdanalysis.org/" target="_blank" class="text-reset"> MDAnalysis</a>, please ensure your files are compatible.',
-                'This form uses the <a href="https://biosim-schema.readthedocs.io/" target="_blank" class="text-reset"> biosim-schema</a> to define and group biosim metadata. Missing terms or units? Please considering raising an <a href="https://github.com/CCPBioSim/biosim-schema/issues" target="_blank" class="text-reset"> issue</a> or contributing to the schema.',
-                'Automatically add metadata associated with each entry using the "Extract metadata" button. Please fill in any missing metadata fields manually where applicable, click on each field name for further information about data field requirements.',
-                "Two options are available for your extracted simulation metadata: <ol> <li> Download the simulation metadata as a json file and share alongside simulation files. </li> <li> Submit your simulation files and metadata in one step to BioSimDB, you will be directed to a login page and asked to authorize this application. Please continue and complete the data submission process in BioSimDB. </li> </ol>",
-            ],
-        },
+        "guidance": [
+            {
+                "label": "Instructions:",
+                "options": [
+                    "To extract simulation metadata, please upload a topology and corresponding trajectory file/s.",
+                    # Optionally, upload an aiida archive file containing the simulation provenance, to extract further information about the simulation protocol.",
+                    "Multiple trajectory files can be uploaded with a single associated topology file, both file types are required.",
+                    'Various simulation file formats are accepted and read using <a href="https://www.mdanalysis.org/" target="_blank" class="text-reset"> MDAnalysis</a>, please ensure your files are compatible.',
+                    'This form uses the <a href="https://biosim-schema.readthedocs.io/" target="_blank" class="text-reset"> biosim-schema</a> to define and group biosim metadata. Missing terms or units? Please consider raising an <a href="https://github.com/CCPBioSim/biosim-schema/issues" target="_blank" class="text-reset"> issue</a> or contributing to the schema.',
+                    'Automatically add metadata associated with each entry using the "Extract metadata" button.',
+                    "Please fill in any missing metadata fields manually where applicable, click on each field name for further information about data field requirements.",
+                    "Two options are available for your extracted simulation metadata: <ol> <li> Download the simulation metadata as a json file and share alongside simulation files. </li> <li> Submit your simulation files and metadata in one step to BioSimDB, you will be directed to a login page and asked to authorize this application. Please continue and complete the data submission process in BioSimDB. </li> </ol>",
+                    "Clear all fields when uploading new files, as manually filled in fields are preserved unless cleared.",
+                ],
+            },
+            {
+                "label": "Any Problems?",
+                "options": [
+                    "This deposition form is in active development, with new features being added, there may be a few bugs &#128030; that have crept in!",
+                    'If you experience any errors or warnings when using this form, please report them <a href="https://github.com/CCPBioSim/biosimdb-interface/issues" target="_blank" class="text-reset"> here</a>.',
+                ],
+            },
+        ],
         "repeatable": True,
         "max": 1,
         "files": {
@@ -72,10 +84,12 @@ WEBFORM_SCHEMA = {
                 "multiple": True,
                 "required": True,
             },
-            "aiida": {
-                "label": "Upload AiiDA archive file",
-                "multiple": False,
-            },
+            # Future option to include aiida archive files
+            # for extraction.
+            # "aiida": {
+            #     "label": "Upload AiiDA archive file",
+            #     "multiple": False,
+            # },
         },
     }
 }
